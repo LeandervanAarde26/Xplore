2cb54bc5903caf7a3f2f8f80ff61775d9c198e7b
epicYellow <105212574+epicYellow@users.noreply.github.com>
2023-06-26 10:28:28 +0200

    Login View created

---
+++ Xplore/Xplore/Login_View.swift
@@ -0,0 +1,20 @@
+//
+//  Login_View.swift
+//  Xplore
+//
+//  Created by Reinhardt de Beer on 2023/06/26.
+//
+
+import SwiftUI
+
+struct Login_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Login_View()
+    }
+}
