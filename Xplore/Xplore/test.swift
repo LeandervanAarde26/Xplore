8b9ef968959c29b7b02e936023c8d22555f8b01e
Leander van Aarde <103223863+LeandervanAarde26@users.noreply.github.com>
2023-06-26 08:29:56 +0200

    Folders added
    Basic folders and structure added to the project, UI creation for today

---
+++ Xplore/Xplore/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  Xplore
+//
+//  Created by Leander van Aarde on 2023/06/26.
+//
+
+import Foundation
---
+++ Xplore/Xplore/tester.swift
@@ -0,0 +1,8 @@
+//
+//  tester.swift
+//  Xplore
+//
+//  Created by Leander van Aarde on 2023/06/26.
+//
+
+import Foundation
