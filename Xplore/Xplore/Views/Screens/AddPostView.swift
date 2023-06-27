9106c16bcf66a168719cade1d14381951ef524a0
Leander van Aarde <103223863+LeandervanAarde26@users.noreply.github.com>
2023-06-27 15:38:10 +0200

    Image Picker complete
    image picker is done

---
+++ Xplore/Xplore/Views/Screens/AddPostView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPostView.swift
+//  Xplore
+//
+//  Created by Leander van Aarde on 2023/06/27.
+//
+
+import SwiftUI
+
+struct AddPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPostView()
+    }
+}
