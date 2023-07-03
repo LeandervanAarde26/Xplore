e5cb9ee81df96a58e51505030711c167685aeaee
Leander van Aarde <103223863+LeandervanAarde26@users.noreply.github.com>
2023-07-03 08:40:04 +0200

    Not sure
    not completely sure

--- Xplore/Xplore/ViewModels/PostViewModel.swift
+++ Xplore/Xplore/ViewModels/PostViewModel.swift
@@ -1,27 +1,45 @@
-//
-//  FirestoreManager.swift
-//  Xplore
-//
-//  Created by Leander Van Aarde on 29/06/2023.
-//
-
 import SwiftUI
 import Firebase
 import FirebaseFirestoreSwift
 
 class PostViewModel: ObservableObject {
     private var db = Firestore.firestore()
-    func addUserPost(userId: String, postImage: String, postDescription: String, postCountry: String){
-        let post = addPost(userId: userId, postImage: postImage, postDescription: postDescription, postCountry: postCountry)
+    @StateObject private var viewModel = ImageUploadViewModel(storageManager: storageManager())
+    
+    func addUserPost(userId: String, postImage: URL?, postDescription: String, postCountry: String) async throws {
+        do {
         
-        let docRef = db.collection("Posts").document("document")
-        
-        docRef.setData(["name" : "Document"]){ error in
-            if let error = error {
-                print ("Error while posting document \(error)")
-            } else{
-                print("Document has been added")
+            guard let imageURL = postImage else {
+                print("Error: Invalid image URL")
+                return
             }
+
+            let image = try await viewModel.uploadImage(fromURL: imageURL)
+
+            
+            print("post here, \(image)")
+            
+//            let post = addPost(userId: userId, postImage: (), postDescription: postDescription, postCountry: postCountry)
+
+//            let docRef = db.collection("Posts").document()
+
+//            let postData: [String: Any] = [
+//                "userId": userId,
+//                "postImage": postImage,
+//                "postDescription": postDescription,
+//                "postCountry": postCountry
+//            ]
+
+//            docRef.setData(postData) { error in
+//                if let error = error {
+//                    print("Error while posting document: \(error)")
+//                } else {
+//                    print("Document has been added")
+//                }
+//            }
         }
+        
+        
+        
     }
 }
